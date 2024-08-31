8ff9f93dc15259c96607c30e0a7ed576e4b7ae63
Piootrekk <piotr.wilkowiecki100@gmail.com>
2024-08-31 21:19:26 +0200

    next, vite react init, tailwind, shadcn cofigurated

---
+++ csr-shop/shop-front/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <Button variant={"outline"} size={"lg"}>
+      Click me
+    </Button>
+  );
+};
+
+export default App;
---
+++ ssr-shop/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <Button variant={"outline"} size={"lg"}>
+      Hello World
+    </Button>
+  );
+};
+
+export default Home;
